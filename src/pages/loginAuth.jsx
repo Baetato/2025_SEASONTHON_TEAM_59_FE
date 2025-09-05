626ba6eaf93870bc2a10cc14db948efbb661cde5
seonyu <sunyu0216@naver.com>
2025-09-05 18:38:44 +0900

    fix(vercel): import 문 수정

--- src/pages/loginAuth.jsx
+++ src/pages/loginAuth.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import LoginTopBar from "../components/loginTopbar";
+import LoginTopBar from "../components/loginTopBar";
 import LoginNextBtn from "../components/loginNextBtn";
 
 export default function LoginAuth() {
