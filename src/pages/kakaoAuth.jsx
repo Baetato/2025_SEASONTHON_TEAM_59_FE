d4fcb0661608b959c2fb49c72da6afae52b6b680
seonyu <sunyu0216@naver.com>
2025-09-06 05:37:38 +0900

    chore: 다시 axios로

--- src/pages/kakaoAuth.jsx
+++ src/pages/kakaoAuth.jsx
@@ -21,7 +21,7 @@ export default function KakaoAuth() {
 
     const fetchToken = async () => {
       try {
-        const res = await api.get(
+        const res = await axios.get(
           `${import.meta.env.VITE_API_BASE_URL}/v1/oauth2/callback/${provider}?code=${code}`
         );
 
