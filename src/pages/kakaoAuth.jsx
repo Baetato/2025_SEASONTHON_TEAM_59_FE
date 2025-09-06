8cf7d9384923f82461783d96279d13f392fd17e4
seonyu <sunyu0216@naver.com>
2025-09-06 12:12:22 +0900

    chore: 백엔드 수정 완료

--- src/pages/kakaoAuth.jsx
+++ src/pages/kakaoAuth.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import api from "../api.js";
-import axios from "axios";
 import styled from "styled-components";
 
 export default function KakaoAuth() {
@@ -21,8 +20,8 @@ export default function KakaoAuth() {
 
     const fetchToken = async () => {
       try {
-        const res = await axios.get(
-          `${import.meta.env.VITE_API_BASE_URL}/v1/oauth2/callback/${provider}?code=${code}`
+        const res = await api.get(
+          `/v1/oauth2/callback/${provider}?code=${code}`
         );
 
         const { accessToken, refreshToken } = res.data.data.tokenDto;
