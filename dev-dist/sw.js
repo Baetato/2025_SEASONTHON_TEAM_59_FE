3318be2022356278a0a2af62f1962c8ddf220944
Baetato <diana021015@gmail.com>
2025-09-16 18:02:05 +0900

    feat: 상점 캐릭터 스킨 백그라운드 제거

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.n584f578d5o"
+    "revision": "0.1bu02vcfv7g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
